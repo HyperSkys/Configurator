df9cf1fea41cf8abf53d9454eb6387850f70ced9
HyperSys <66301237+HyperSkys@users.noreply.github.com>
2022-07-11 13:12:19 -0600

    cowboy 🤠

--- src/main/java/dev/hyperskys/configurator/api/exception/ObjectNotFoundException.java
+++ src/main/java/dev/hyperskys/configurator/api/exception/ObjectNotFoundException.java
@@ -1,3 +1,8 @@
+/**
+ * @author HyperSkys Development
+ * Copyright (C) 2020-2022 - HyperSkys Development
+ */
+
 package dev.hyperskys.configurator.api.exception;
 
 public class ObjectNotFoundException extends RuntimeException {
